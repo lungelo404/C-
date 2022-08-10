7626fc7a2492fb5719f6baf695588c490f7522b7
lungelo404 <59869421+lungelo404@users.noreply.github.com>
2022-08-10 18:59:47 +0200

    Prime numbers
    Prime numbers

---
+++ Prime numbers/Prime numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = 0;
+
+            for (int i = 0; count < 100; i++)
+            {
+                if (primeNumber(i))
+                {
+                    Console.WriteLine("{0}) {1}", ++count, i);
+                }
+            }
+        }
+
+
+        static bool primeNumber(int number)
+        {
+            bool result = true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            if (number <= 1)
+            {
+                result = false;
+
+            }
+            return result;
+        }
+    }
+}
+
+
+
+
+
+
